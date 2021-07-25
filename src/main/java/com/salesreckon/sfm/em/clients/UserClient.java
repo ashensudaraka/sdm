a16066964a407b5b6f42be953cc5cad5fa2b593e
gihan.k <gihan.k@salesreckon.com>
2021-07-25 10:40:48 +0530

    Added users client caching.

--- src/main/java/com/salesreckon/sfm/em/clients/UserClient.java
+++ src/main/java/com/salesreckon/sfm/em/clients/UserClient.java
@@ -2,6 +2,7 @@ package com.salesreckon.sfm.em.clients;
 
 import com.salesreckon.sfm.em.domain.UserRepresentation;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "UserClient", url = "http://um/users")
 public interface UserClient {
+    @Cacheable("users")
     @GetMapping("{id}")
     public ResponseEntity<UserRepresentation> get(@PathVariable String id);
 }
