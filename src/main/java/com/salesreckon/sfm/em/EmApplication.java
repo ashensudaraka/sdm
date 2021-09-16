aa52f362b182641813ff2ad483db2d326d9e18f6
gihan.k <gihan.k@salesreckon.com>
2021-09-16 11:39:52 +0530

    New core

--- src/main/java/com/salesreckon/sfm/em/EmApplication.java
+++ src/main/java/com/salesreckon/sfm/em/EmApplication.java
@@ -2,12 +2,17 @@ package com.salesreckon.sfm.em;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
 
 @EnableCaching
 @EnableFeignClients
-@SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+@EntityScan({"com.salesreckon.microservices.core", "com.salesreckon.sfm.em"})
+@SpringBootApplication(scanBasePackages = {"com.salesreckon.sfm.em", "com.salesreckon.microservices.core"})
 public class EmApplication {
 
 	public static void main(String[] args) {
